d5fdaf9f3a6ad1819114f22ea35080b82610d4d5
umash <umash@DESKTOP-DICTBGJ>
2022-09-19 18:30:25 +0530

    first commit

---
+++ StringDependencyInjection/src/main/java/com/tns/di/Student.java
@@ -0,0 +1,15 @@
+package com.tns.di;
+
+public class Student {
+	
+MathCheat chit;
+
+public void setChit(MathCheat chit) {
+	this.chit = chit;
+}
+
+public void cheating()
+ {
+	 chit.mathCheat();
+ }
+}
